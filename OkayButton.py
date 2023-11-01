a97c321f38722c50e33e2840f96a9dea85595719
vossjona <123163559+vossjona@users.noreply.github.com>
2023-11-01 18:10:34 +0100

    Add python file
    Contains basic test for a GUI

---
+++ OkayButton.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 30 13:48:31 2022
+
+@author: Jonas
+"""
+
+import tkinter as tk
+
+def on_button_click():
+    # Exit the GUI
+    root.destroy()
+
+# Create the main window
+root = tk.Tk()
+
+# Create a button
+button = tk.Button(text="Okay!", command=on_button_click)
+button.pack()
+
+# Run the GUI
+root.mainloop()
